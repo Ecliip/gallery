f146e26b14c6ea8bc9c2027f2b79da378ad40a01
Micaelo <prodomitor@gmail.com>
2020-12-15 22:03:34 +0100

    routes

--- resources/views/pages/painting.blade.php
+++ resources/views/pages/painting.blade.php
@@ -1,4 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Gallery</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
+    <!-- Fonts -->
 
+    <!-- Styles -->
+
+</head>
     <div class="flex flex-col items-center justify-center">
         <h1>{{$painting->title}}</h1>
         <div class="">
@@ -14,3 +26,4 @@
         </div>
     </div>
 
+</html>
